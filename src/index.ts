60398b39e2f286056773233fa57fc6ae30adcc98
Pornpimol <pornpimol_ka@cmu.ac.th.com>
2025-12-05 18:58:02 +0700

    task 6 step 4-5

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-
-// console.log('type of X: ',typeof x);
-
-let x:number | string = 10;
-// x = 'hello';
-if (typeof x === 'string') {
-   console.log('x is a string');
-}else if (typeof x === 'number') {
-   console.log('x is a number');
-}else {
-   console.log('x is neither a string nor a number');
-}
+let i = 0
+i = 'hello' as any
+console.log(i)
