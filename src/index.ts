f7f7721acfd2dfcca205469fab29319fe2c1d19a
Pornpimol <pornpimol_ka@cmu.ac.th.com>
2025-12-05 18:55:04 +0700

    Tassk 6 step1-3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, TypeScript with Node.js!");
+
+// console.log('type of X: ',typeof x);
+
+let x:number | string = 10;
+// x = 'hello';
+if (typeof x === 'string') {
+   console.log('x is a string');
+}else if (typeof x === 'number') {
+   console.log('x is a number');
+}else {
+   console.log('x is neither a string nor a number');
+}
