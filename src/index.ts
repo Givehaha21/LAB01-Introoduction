9a77f9ad6e45b9cc66ad2bd9b31a181d9a1a659b
Pornpimol <pornpimol_ka@cmu.ac.th.com>
2025-12-05 19:01:45 +0700

    task 7 step 1-2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-let i = 0
-i = 'hello' as any
-console.log(i)
+//let สามารถเปลี่ยนค่าได้
+let a = 10;
+a = 15;
+console.log(a);
+
+//cost ไม่สามารถเปลี่ยนค่าได้
+const x = 10;
+// x = 15;
+console.log(x);
