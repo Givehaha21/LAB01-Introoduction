d60a095c99b20894aec9fe13d0f6c11f724177fe
Pornpimol <pornpimol_ka@cmu.ac.th.com>
2025-12-05 19:20:22 +0700

    task 7 step 5

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ interface Person {
    name: string;
    age: number;
 }
+
 const a: Person = {
    name: 'Alice',
    age: 30
 }
-// console.log(`Name: ${a.name}, Age: ${a.age}`);
-a = {
-   name: 'Bob',
-   age: 25
-}
-console.log(a);
+
+a.name = 'Bob'
+a.age = 25
+console.log(a)
+
 
