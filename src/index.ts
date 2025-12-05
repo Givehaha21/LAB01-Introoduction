770d1887d9ffe1d6c8906f98df3d2ec63296178e
Pornpimol <pornpimol_ka@cmu.ac.th.com>
2025-12-05 19:18:26 +0700

    task 7 step 3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-//let สามารถเปลี่ยนค่าได้
-let a = 10;
-a = 15;
+interface Person {
+   name: string;
+   age: number;
+}
+let a: Person = {
+   name: 'Alice',
+   age: 30
+}
+// console.log(`Name: ${a.name}, Age: ${a.age}`);
+a = {
+   name: 'Bob',
+   age: 25
+}
 console.log(a);
 
-//cost ไม่สามารถเปลี่ยนค่าได้
-const x = 10;
-// x = 15;
-console.log(x);
