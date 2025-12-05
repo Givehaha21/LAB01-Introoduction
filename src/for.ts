c5f680ceef457e56f497fdc66ac5f7304cb3e018
Pornpimol <pornpimol_ka@cmu.ac.th.com>
2025-12-05 20:45:48 +0700

    task 11 step 1-3

--- src/for.ts
+++ src/for.ts
@@ -1,10 +1,5 @@
-// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']  
-// for (const c of color) {  
-//   console.log(c);  
-// } 
-
-
-let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']  
-for (const index in color) {  
-  console.log(`Index: ${index}, Value: ${color[index]}`);  
-} 
+function greet(name: string) {
+    // console.log('Hello ' + name);
+    return 'Hello ' + name
+}
+console.log(greet('John'));
